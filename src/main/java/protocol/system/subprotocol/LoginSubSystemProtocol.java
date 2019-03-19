6e8664fd4d0908a3c73e34afb752c1fed17e5d4f
yechanpark <qkrdpcks0419@naver.com>
2019-03-19 16:35:33 +0900

    로그인 시 접속한 유저(자신 포함) 리스트 받아서 출력하는 기능 추가

--- src/main/java/protocol/system/subprotocol/LoginSubSystemProtocol.java
+++ src/main/java/protocol/system/subprotocol/LoginSubSystemProtocol.java
@@ -1,5 +1,7 @@
 package protocol.system.subprotocol;
 
+import java.util.List;
+
 import client.frame.GameFrame;
 import client.frame.LoginFrame;
 import exception.game.login.LoginFailureException;
@@ -11,6 +13,7 @@ public class LoginSubSystemProtocol extends SystemProtocol {
     private String password;
     private boolean isLoginSuccess;
     private String loginFailedReason;
+    private List<String> loginUsers; // 이미 로그인한 유저의 리스트
 
     public String getUserId() {
         return userId;
@@ -48,6 +51,15 @@ public class LoginSubSystemProtocol extends SystemProtocol {
         return this;
     }
 
+    public List<String> getLoginUsers() {
+		return loginUsers;
+	}
+
+	public LoginSubSystemProtocol setLoginUsers(List<String> loginUsers) {
+		this.loginUsers = loginUsers;
+		return this;
+	}
+	
     @Override
     public void execute() {
         System.out.println(this.getClass().getSimpleName() + ".execute()");
@@ -57,13 +69,15 @@ public class LoginSubSystemProtocol extends SystemProtocol {
         
         User user = User.getInstance();
         
-        // 자기 자신에 대한 로그인 메시지인 경우
+        // 자기 자신에 대한 로그인 메시지인 경우 GameFrame을 띄움
         if(user.getUserId().equals(this.userId)) {
 	        user.setUserId(this.userId);
 	        LoginFrame.getInstance().setVisible(false);
 	        GameFrame.getInstance().boot();
         }
         
+        // 특정 유저가 로그인 했다는 알림
         GameFrame.getInstance().appendMessageToTextPane(this.userId + " login");
+        GameFrame.getInstance().appendMessageToTextPane("login users : " + this.loginUsers.toString());
     }
 }
